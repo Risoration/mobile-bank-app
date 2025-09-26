a75c7ac6c893f50c1f0ee400a3177c8794002098
Christos Lykoudis <clykoudisc@gmail.com>
2025-09-26 14:32:32 +0100

    browser routing fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Register from './pages/Register';
 import { Home } from './pages/Home';
@@ -25,57 +25,55 @@ const App: React.FC = () => {
   const [modalView, setModalView] = useState<ModalView>('input');
 
   return (
-    <BrowserRouter>
-      <UserContextProvider>
-        <SettingsProvider>
-          <ThemeProvider>
-            <div className='bg-[rgb(var(--color-theme-background))] text-[color:rgb(var(--color-theme-text-primary))] max-h-screen overflow-y-auto'>
-              <Navbar
-                openLogin={() => {
-                  setModalView('login');
-                  setModalOpen(true);
-                }}
+    <UserContextProvider>
+      <SettingsProvider>
+        <ThemeProvider>
+          <div className='bg-[rgb(var(--color-theme-background))] text-[color:rgb(var(--color-theme-text-primary))] max-h-screen overflow-y-auto'>
+            <Navbar
+              openLogin={() => {
+                setModalView('login');
+                setModalOpen(true);
+              }}
+            />
+
+            <Toaster
+              position='bottom-right'
+              toastOptions={{ duration: 2000 }}
+            />
+
+            <Routes>
+              <Route
+                path='/'
+                element={<Home />}
               />
-
-              <Toaster
-                position='bottom-right'
-                toastOptions={{ duration: 2000 }}
+              <Route
+                path='/home'
+                element={<Home />}
               />
+              <Route
+                path='/main'
+                element={<MainPage />}
+              />
+            </Routes>
 
-              <Routes>
-                <Route
-                  path='/'
-                  element={<Home />}
+            <Modal
+              isOpen={modalOpen}
+              onClose={() => setModalOpen(false)}
+            >
+              {modalView === 'login' && (
+                <Login
+                  switchToRegister={() => setModalView('register')}
+                  setModalOpen={setModalOpen}
                 />
-                <Route
-                  path='/home'
-                  element={<Home />}
-                />
-                <Route
-                  path='/main'
-                  element={<MainPage />}
-                />
-              </Routes>
-
-              <Modal
-                isOpen={modalOpen}
-                onClose={() => setModalOpen(false)}
-              >
-                {modalView === 'login' && (
-                  <Login
-                    switchToRegister={() => setModalView('register')}
-                    setModalOpen={setModalOpen}
-                  />
-                )}
-                {modalView === 'register' && (
-                  <Register switchToLogin={() => setModalView('login')} />
-                )}
-              </Modal>
-            </div>
-          </ThemeProvider>
-        </SettingsProvider>
-      </UserContextProvider>
-    </BrowserRouter>
+              )}
+              {modalView === 'register' && (
+                <Register switchToLogin={() => setModalView('login')} />
+              )}
+            </Modal>
+          </div>
+        </ThemeProvider>
+      </SettingsProvider>
+    </UserContextProvider>
   );
 };
 
